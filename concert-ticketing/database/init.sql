def814866a7747fbb73e8984e5dabd6762afd487
ashmus <ashmus.codes@gmail.com>
2026-03-12 14:54:28 +0800

    code updated

--- concert-ticketing/database/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE events_db;
-CREATE DATABASE payment_db;
-CREATE DATABASE seat_allocation_db;
-CREATE DATABASE swap_db;
--- concert-ticketing/services/notification/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
-RABBITMQ_PORT = os.getenv("RABBITMQ_PORT", "5672")
