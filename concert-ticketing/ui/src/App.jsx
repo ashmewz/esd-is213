251b3baf48d45f7d8a796fc68a05a72a451bda20
yujjjia <yujia.ngm.2024@smu.edu.sg>
2026-03-18 15:41:13 +0800

    set up react ui with vite and tailwind

---
+++ concert-ticketing/ui/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-gray-900 flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Concert Ticketing</h1>
+    </div>
+  )
+}
+
+export default App
